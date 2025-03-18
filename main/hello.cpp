b25be07160a4ac414a9fe79a55cabb056127cd0a
Prabhat Deshmukh <prabhat.deshmukh22@gmail.com>
2025-03-18 15:23:46 +0530

    Commiting PES1UG22CS419 Jenkins yes

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World My name is Prabhat and my SRN is PES1UG22CS419\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
